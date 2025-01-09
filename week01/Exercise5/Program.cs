a5e238d6b926ae40cd30fe6554b9afcbd280b635
moronialanis <moronialanis23@gmail.com>
2025-01-08 22:04:14 -0800

    change

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Xml.XPath;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+       
+        
+
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int SquaredNumber = SquareNumber(userNumber);
+        
+        DisplayResult(userName, SquaredNumber);
+
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program! ");
+    }
+
+
+    static string PromptUserName()
+    {
+       
+        Console.Write("Please enter your name: ");
+        string names = Console.ReadLine();
+        return names;
+    }
+
+
+    static int PromptUserNumber()
+    {
+       Console.Write("Please enter your favorite number: ");
+       int number = int.Parse(Console.ReadLine());
+       return number;
+    }
+
+
+
+    static int SquareNumber (int number)
+    {
+       int Square = number * number;
+       return Square;
+    }
+
+
+   static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+        
+    }
+
+
 }
+
+
