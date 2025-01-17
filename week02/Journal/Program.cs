b89a9414f0712bea0f1be490851a01de2269469a
moronialanis <moronialanis23@gmail.com>
2025-01-16 16:29:43 -0800

    change

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
     }
 }
