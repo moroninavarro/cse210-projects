fd2443083879140d2ec6423a6abd40655c8293e7
moronialanis <moronialanis23@gmail.com>
2025-01-20 22:40:52 -0800

    change

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,42 @@
+using System.Reflection.Emit;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+
+    }
+
+    public Fraction(int WholeNumber)
+    {
+        _top = WholeNumber;
+        _bottom = 1;
+
+    }
+
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+       return (double)_top / (double)_bottom;
+    }
+
+}
