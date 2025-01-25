a2a8d27d83d3f01a499c7d22ccbf1811175c7bef
moronialanis <moronialanis23@gmail.com>
2025-01-24 20:47:34 -0800

    change

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,10 +1,17 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
+// I used Meta AI to do this assignment.
+//Exceeding Requirements: I added a stopwatch to know the time that you taked to be able to memorize the scripture.
+//I think that this could be like a game to have more fun.
 class Program
-{
+{   
+   
     static void Main(string[] args)
-    {
+    {   
+
+       DateTime startTime = DateTime.Now;
 
         Scripture escritura = new Scripture(new Reference("Proverbios", 3, 5, 6),"Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
         
@@ -37,12 +44,10 @@ class Program
                 Console.ReadLine();
                 break;
             }
-
-           
-           
         }
 
-        
-        
+        DateTime endTime = DateTime.Now;
+        TimeSpan elapsedTime = endTime - startTime;
+        Console.WriteLine("Time you have spend on the program:  " + elapsedTime.ToString(@"hh\:mm\:ss"));
     }
 }
