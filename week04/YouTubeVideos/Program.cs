d1590d8e8c9810c7a054f9cb0ab5755ddb502149
moronialanis <moronialanis23@gmail.com>
2025-01-31 16:31:21 -0800

    change

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -2,8 +2,15 @@ using System;
 
 class Program
 {
+    static Video TheVideo = new Video();
     static void Main(string[] args)
     {
+
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        
+        Console.WriteLine(TheVideo);
     }
+
+
+    
 }
--- week04/YouTubeVideos/video.cs
+++ week04/YouTubeVideos/video.cs
@@ -1,12 +1,29 @@
 using System;
+using System.Transactions;
 
 public class Video {
 
-    static Comment comentarios = new Comment();
+    static Comment comentarios = new Comment("", "");
+    private List<Comment> _comment;
+
+
     public string _title;
     public string _author;
     public int _seconds;
 
-    
+   // Each video also has responsibility to store a list of comments, and should have 
+   //a method to return the number of comments. 
    
+
+   public Video(){
+    _comment = new List<Comment>();
+   }
+
+   public void AddComment(Comment comment){
+    _comment.Add(comment);
+   }
+
+   public int NumComments(){
+    return _comment.Count;
+   }
 }
